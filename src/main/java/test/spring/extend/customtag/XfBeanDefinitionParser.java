1c885788dc09b233973524d5c2fc23fd0d5d8cb8
yangqf <yangqf@shishike.com>
2016-06-07 10:51:53 +0800

    spring xml扩展例子

--- src/main/java/test/spring/extend/customtag/XfBeanDefinitionParser.java
+++ src/main/java/test/spring/extend/customtag/XfBeanDefinitionParser.java
@@ -14,6 +14,7 @@ import org.w3c.dom.Element;
 public class XfBeanDefinitionParser implements BeanDefinitionParser{
     @Override
     public BeanDefinition parse(Element element, ParserContext parserContext){
+        Object o = parserContext.extractSource(element);
         String age = element.getAttribute("age");
         String name = element.getAttribute("name");
 
